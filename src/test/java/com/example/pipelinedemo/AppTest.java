df12428ecdfdce3711a2f3b3d7bc3ebc9935678e
kameshsaripaka <saripaka.kamesh@gmail.com>
2026-04-11 05:33:23 +0530

    Vaishnavi - After Adding New Date

--- src/test/java/com/example/pipelinedemo/AppTest.java
+++ src/test/java/com/example/pipelinedemo/AppTest.java
@@ -35,7 +35,7 @@ public class AppTest {
         driver.get("https://www.google.com");
         assertTrue(driver.getTitle().contains("Google"), "Title should contain Google");
         System.out.println("Today's date is: " + new java.util.Date());
-        System.out.println("Diana Anu Test case April 6th....");
+        System.out.println("Vaishnavi's Test Case Execution .... Google Home Page");
         System.out.println("Page title: " + driver.getTitle());
         if(driver.getTitle().contains("Google")) 
         {
